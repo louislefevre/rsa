d727c565c5edda76e868bdb942834baf41796a8e
Louis Lefevre <LouisLefev@gmail.com>
2020-02-23 03:48:19 +0000

    Add random prime generation
    Prime numbers are securely generated for variables p and q.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,39 @@
+import java.security.SecureRandom;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        long p = generateRandomPrimes(5000);
+        long q = generateRandomPrimes(5000);
+        System.out.println(p);
+        System.out.println(q);
+    }
+
+    static long generateRandomPrimes(int limit)
+    {
+        SecureRandom random = new SecureRandom();
+
+        while(true)
+        {
+            long randomNumber = random.nextInt(limit);
+
+            if(!isPrime(randomNumber))
+                continue;
+
+            return randomNumber;
+        }
+    }
+
+    static boolean isPrime(long num)
+    {
+        if(num <= 1)
+            return false;
+
+        for(int i = 2; i < num; i++)
+            if(num % i == 0)
+                return false;
+
+        return true;
     }
 }
