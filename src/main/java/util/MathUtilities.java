5b900fa8fa22e6490349b19052bb3861a94ea034
Louis Lefevre <LouisLefev@gmail.com>
2020-03-08 02:00:03 +0000

    Cleanup MathUtilities.java

--- src/main/java/util/MathUtilities.java
+++ src/main/java/util/MathUtilities.java
@@ -4,9 +4,28 @@ import java.security.SecureRandom;
 
 public final class MathUtilities
 {
-    public MathUtilities()
+    private MathUtilities()
     {
         throw new UnsupportedOperationException();
+    }
+
+    public static int generateRandomNumber()
+    {
+        SecureRandom random = new SecureRandom();
+        return random.nextInt(100);
+    }
+
+    public static boolean isPrime(int num)
+    {
+        // https://mkyong.com/java/how-to-determine-a-prime-number-in-java/
+        if(num <= 1 || num % 2 == 0)
+            return false;
+
+        for(int i = 3; i*i <= num; i+=2)
+            if(num % i == 0)
+                return false;
+
+        return true;
     }
 
     public static int findInverse(int b, int a)
@@ -31,21 +50,17 @@ public final class MathUtilities
         if(sign==-1)
             s=b-s;
 
-        int answer=(r-s)%store;
-
-        return answer;
+        return (r-s)%store;
     }
 
     public static boolean isCoPrime(int e, int r)
     {
-        // https://www.geeksforgeeks.org/check-two-numbers-co-prime-not/
-        if(GCD(e, r) == 1)
-            return true;
-        return false;
+        return GCD(e, r) == 1;
     }
 
     private static int GCD(int a, int b)
     {
+        // https://www.geeksforgeeks.org/check-two-numbers-co-prime-not/
         if(a == 0 || b == 0)
             return 0;
         if(a == b)
@@ -53,24 +68,5 @@ public final class MathUtilities
         if(a > b)
             return GCD(a-b, b);
         return GCD(a, b-a);
-    }
-
-    public static boolean isPrime(int num)
-    {
-        // https://mkyong.com/java/how-to-determine-a-prime-number-in-java/
-        if(num <= 1 || num % 2 == 0)
-            return false;
-
-        for(int i = 3; i*i <= num; i+=2)
-            if(num % i == 0)
-                return false;
-
-        return true;
-    }
-
-    public static int generateRandomNumber()
-    {
-        SecureRandom random = new SecureRandom();
-        return random.nextInt(100);
     }
 }
