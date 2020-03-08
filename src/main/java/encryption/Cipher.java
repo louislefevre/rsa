8ba987a14f42238640ee2f1ebe394491ae7ebbba
Louis Lefevre <LouisLefev@gmail.com>
2020-03-08 03:53:50 +0000

    Rework encryption conversion
    Before when the message was encrypted, the encrypted ASCII values were stored in
    an array. However, now these values are instead turned back into characters and appended to a string.

--- src/main/java/encryption/Cipher.java
+++ src/main/java/encryption/Cipher.java
@@ -3,10 +3,7 @@ package encryption;
 import keys.PrivateKey;
 import keys.PublicKey;
 import keys.RSAKeyPair;
-import util.TextUtilities;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public final class Cipher
@@ -23,22 +20,12 @@ public final class Cipher
         PublicKey publicKey = keyPair.getPublicKey();
         PrivateKey privateKey = keyPair.getPrivateKey();
 
-        // Convert the input message to a list of ASCII characters.
-        List<Integer> asciiMessage = TextUtilities.stringToAsciiList(message);
-
-        // Encrypt each individual character and store the result in encryptedArray.
-        List<Integer> encryptedArray = encryptCharacters(asciiMessage, publicKey);
-
-        // Decrypt each individual character and store the result in decryptedArray.
-        List<Integer> decryptedArray = decryptCharacters(encryptedArray, privateKey);
-
-        // Convert the decrypted characters back into a string.
-        String decryptedMessage = TextUtilities.asciiListToString(decryptedArray);
+        // Encrypt and decrypt the message.
+        String encryptedMessage = encryptText(message, publicKey);
+        String decryptedMessage = decryptText(encryptedMessage, privateKey);
 
         System.out.println("Original Message: " + message);
-        System.out.println("ASCII Message: " + asciiMessage);
-        System.out.println("Encrypted Array: " + encryptedArray);
-        System.out.println("Decrypted Array: " + decryptedArray);
+        System.out.println("Encrypted Message: " + encryptedMessage);
         System.out.println("Decrypted Message: " + decryptedMessage);
     }
 
@@ -49,25 +36,31 @@ public final class Cipher
         return scanner.nextLine();
     }
 
-    private static List<Integer> encryptCharacters(List<Integer> charArray, PublicKey publicKey)
+    private static String encryptText(String text, PublicKey publicKey)
     {
-        List<Integer> encryptedArray = new ArrayList<>();
-        for(int asciiChar : charArray)
+        char[] charArray = text.toCharArray();
+        StringBuilder encryptedText = new StringBuilder();
+
+        for(char letter : charArray)
         {
-            int encryptedCharacter = RSA.encrypt(asciiChar, publicKey);
-            encryptedArray.add(encryptedCharacter);
+            int encryptedCharacter = RSA.encrypt(letter, publicKey);
+            encryptedText.append((char) encryptedCharacter);
         }
-        return encryptedArray;
+
+        return encryptedText.toString();
     }
 
-    private static List<Integer> decryptCharacters(List<Integer> charArray, PrivateKey privateKey)
+    private static String decryptText(String text, PrivateKey privateKey)
     {
-        List<Integer> decryptedArray = new ArrayList<>();
-        for(int encryptedAsciiChar : charArray)
+        char[] charArray = text.toCharArray();
+        StringBuilder decryptedText = new StringBuilder();
+
+        for(char letter : charArray)
         {
-            int decryptedCharacter = RSA.decrypt(encryptedAsciiChar, privateKey);
-            decryptedArray.add(decryptedCharacter);
+            int shiftedLetter = RSA.decrypt(letter, privateKey);
+            decryptedText.append((char) shiftedLetter);
         }
-        return decryptedArray;
+
+        return decryptedText.toString();
     }
 }
