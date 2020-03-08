18330972f60f81849fff9a2f50f7e8a782ac7b2c
Louis Lefevre <LouisLefev@gmail.com>
2020-03-08 02:45:53 +0000

    Add text encryption
    Text can now be encrypted by converting characters to an ASCII array.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,36 +3,67 @@ import keys.PrivateKey;
 import keys.PublicKey;
 import keys.RSAKeyPair;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        RSAKeyPair keyPair = new RSAKeyPair();
+        String message = readInput();
+        if(message.isBlank())
+            return;
 
+        byte[] asciiMessage = toASCII(message);
+
+        RSAKeyPair keyPair = new RSAKeyPair();
         PublicKey publicKey = keyPair.getPublicKey();
         PrivateKey privateKey = keyPair.getPrivateKey();
-
-        int message = 56;
-
+        
         RSA rsa = new RSA();
-        int encryptedMessage = rsa.encrypt(message, publicKey);
-        int decryptedMessage = rsa.decrypt(encryptedMessage, privateKey);
+
+        List<Integer> encryptedArray = new ArrayList<>();
+        for(byte character : asciiMessage)
+        {
+            int encryptedCharacter = rsa.encrypt(character, publicKey);
+            encryptedArray.add(encryptedCharacter);
+        }
+
+        List<Integer> decryptedArray = new ArrayList<>();
+        for(int value : encryptedArray)
+        {
+            int decryptedCharacter = rsa.decrypt(value, privateKey);
+            decryptedArray.add(decryptedCharacter);
+        }
+
+        String decryptedMessage = toCharacters(decryptedArray);
 
         System.out.println("Original Message: " + message);
-        System.out.println("Encrypted Message: " + encryptedMessage);
+        System.out.println("Encrypted Array: " + encryptedArray);
+        System.out.println("Decrypted Array: " + decryptedArray);
         System.out.println("Decrypted Message: " + decryptedMessage);
     }
+
+    private static String readInput()
+    {
+        System.out.print("> ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private static byte[] toASCII(String text)
+    {
+        return text.getBytes();
+    }
+
+    private static String toCharacters(List<Integer> list)
+    {
+        StringBuilder text = new StringBuilder();
+        for(int value : list)
+        {
+            text.append((char) value);
+        }
+        return text.toString();
+    }
 }
-
-/*
-* n = modulus
-* */
-
-
-//        System.out.println("p: "+p);
-//        System.out.println("q: "+q);
-//        System.out.println("n: "+n);
-//        System.out.println("r: "+r);
-//        System.out.println("e: "+e);
-//        System.out.println("d: "+d);
-//        System.out.println("---");
--- src/main/java/encryption/RSA.java
+++ src/main/java/encryption/RSA.java
@@ -7,16 +7,15 @@
  *    The number e is used as the public key exponent.
  * 5. Compute a number d such that: d*e = 1 (mod φ(n)). The number d is used as the private key exponent.
  *
- * NOTES: - Primes might need to be different numbers.
- *        - Internally, this method works only with numbers (no text), which are between 0 and n.
+ * NOTES: - Internally, this method works only with numbers (no text), which are between 0 and n.
  *        - It could be the case that the message >= modulus. To avoid, use a do-while loop to generate key
- *         until modulus happen to be exactly N bits.
- *       - It's possible that gcd(phi, publicKey) != 1 in which case the key generation fails. This will only
- *         happen if phi is a multiple of 65537. To avoid, use a do-while loop to generate keys until the gcd is 1.
- *       - All messages should be divided into a number of parts. Then, each part should be converted to a number
- *         (that must be larger than 0 and smaller than n). In practice, the message should be divided into fragments
- *         of the size of a certain number of bits.
- *       - Use the library BigInteger to work with big numbers.
+ *          until modulus happen to be exactly N bits.
+ *        - It's possible that gcd(phi, publicKey) != 1 in which case the key generation fails. This will only
+ *          happen if phi is a multiple of 65537. To avoid, use a do-while loop to generate keys until the gcd is 1.
+ *        - All messages should be divided into a number of parts. Then, each part should be converted to a number
+ *          (that must be larger than 0 and smaller than n). In practice, the message should be divided into fragments
+ *          of the size of a certain number of bits.
+ *        - Use the library BigInteger to work with big numbers.
  * */
 
 package encryption;
