b07a53df7046ec6641eea1d73f1bfc731282b94f
Louis Lefevre <LouisLefev@gmail.com>
2020-03-09 23:45:59 +0000

    Add key generator methods
    Created individual methods for each piece of functionality involved in key generation.
    
    Changed public key exponent generation by looping through Fermat numbers instead of choosing 65537 by default every time.

--- src/main/java/keys/RSAKeyGenerator.java
+++ src/main/java/keys/RSAKeyGenerator.java
@@ -5,25 +5,57 @@ import java.util.Random;
 
 public final class RSAKeyGenerator
 {
-    public static final int PRIME_BOUND = 65537;
-    public static final int BIT_LENGTH = 2048;
+    public static final short BIT_LENGTH = 2048;
+    public static final int[] FERMAT_NUMBERS = {65537, 257, 17, 5, 3};
 
     public RSAKeyGenerator() { }
 
     public KeyPair generateKeyPair()
     {
-        Random random = new MersenneTwister();
+        BigInteger primeP = generatePrime(BIT_LENGTH);
+        BigInteger primeQ = generatePrime(BIT_LENGTH+2);
 
-        BigInteger primeP = BigInteger.probablePrime(BIT_LENGTH, random);
-        BigInteger primeQ = BigInteger.probablePrime(BIT_LENGTH+2, random);
-        BigInteger modulus = primeP.multiply(primeQ);
-        BigInteger phi = primeP.subtract(BigInteger.ONE).multiply(primeQ.subtract(BigInteger.ONE));
-        BigInteger publicExponent = BigInteger.valueOf(PRIME_BOUND);
-        BigInteger privateExponent = publicExponent.modInverse(phi);
+        BigInteger modulus = generateModulus(primeP, primeQ);
+        BigInteger phi = generatePhi(primeP, primeQ);
+
+        BigInteger publicExponent = generatePublicExponent(phi);
+        BigInteger privateExponent = generatePrivateExponent(publicExponent, phi);
 
         PublicKey publicKey = new PublicKey(publicExponent);
         PrivateKey privateKey = new PrivateKey(privateExponent);
 
         return new KeyPair(publicKey, privateKey, modulus);
+    }
+
+    private static BigInteger generatePrime(int bitLength)
+    {
+        Random random = new MersenneTwister();
+        return BigInteger.probablePrime(bitLength, random);
+    }
+
+    private static BigInteger generateModulus(BigInteger primeOne, BigInteger primeTwo)
+    {
+        return primeOne.multiply(primeTwo);
+    }
+
+    private static BigInteger generatePhi(BigInteger primeOne, BigInteger primeTwo)
+    {
+        return primeOne.subtract(BigInteger.ONE).multiply(primeTwo.subtract(BigInteger.ONE));
+    }
+
+    private static BigInteger generatePublicExponent(BigInteger phi)
+    {
+        for(int value : FERMAT_NUMBERS)
+        {
+            BigInteger fermatNumber = BigInteger.valueOf(value);
+            if(phi.gcd(fermatNumber).equals(BigInteger.ONE))
+                return fermatNumber;
+        }
+        return BigInteger.valueOf(FERMAT_NUMBERS[4]);
+    }
+
+    private static BigInteger generatePrivateExponent(BigInteger exponent, BigInteger phi)
+    {
+        return exponent.modInverse(phi);
     }
 }
