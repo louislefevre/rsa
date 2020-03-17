115d17fb7e713f8132a80e9091c957a65b1a462f
Louis Lefevre <louislefev@gmail.com>
2020-03-17 17:23:37 +0000

    Add comments
    Added comments to main interface, RSA encryption/decryption and key
    generation.

--- src/main/java/gui/MainInterface.java
+++ src/main/java/gui/MainInterface.java
@@ -13,12 +13,19 @@ public final class MainInterface extends JFrame
 
     private void assembleInterface()
     {
+        /* Sets the title of the application, which will appear at the top of the interface. */
         String title = "RSA Algorithm";
         super.setTitle(title);
 
+        /* Retrieves the image file from the resources directory and sets it to be the applications
+         * image icon. This imagine appears in both the task-bar and next to the title at the top of
+         * the interface. */
         Image image = Toolkit.getDefaultToolkit().getImage("src/main/resources/icon.png");
         super.setIconImage(image);
 
+        /* The tabPane represents selection bar that consists of various tabs. Each of these tabs
+         * are added to the tabPane by passing their title and layout creation methods.
+         * note that the call to pack() is required to correctly adjust the size of the interface contents. */
         JTabbedPane tabPane = new JTabbedPane();
         GenerateKeysTab keysTab = new GenerateKeysTab();
         EncryptTab encryptTab = new EncryptTab();
@@ -29,12 +36,16 @@ public final class MainInterface extends JFrame
         super.add(tabPane);
         super.pack();
 
+        /* Retrieves the users screen resolution dimensions, and adjusts the size of the
+         * interface to be 50% of the width and 90% of the height. The location of the interface
+         * is also set to appear in the centre of the screen. */
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double width = screenSize.getWidth() * 0.5;
         double height = screenSize.getHeight() * 0.9;
         super.setSize((int)width, (int)height);
         super.setLocationRelativeTo(null);
 
+        /* Displays the interface once all the setup is done.*/
         super.setVisible(true);
     }
 }
